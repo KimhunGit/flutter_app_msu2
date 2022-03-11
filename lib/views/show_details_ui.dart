9e5fbc542dd69e826fd33186d48a82285695a544
KimhunGit <jakkrid.b@msu.ac.th>
2022-03-11 15:25:34 +0700

    Code V1.5: Project Day 3 11 Mar 65

--- lib/views/show_details_ui.dart
+++ lib/views/show_details_ui.dart
@@ -1,17 +1,190 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ShowDetailsUI extends StatefulWidget {
-  const ShowDetailsUI({Key? key}) : super(key: key);
+  String? name_fs, web_fs, facebook_fs, phone_fs, image_fs, lat_fs, lng_fs;
+
+  ShowDetailsUI({
+    Key? key,
+    this.name_fs,
+    this.web_fs,
+    this.facebook_fs,
+    this.phone_fs,
+    this.image_fs,
+    this.lat_fs,
+    this.lng_fs,
+  }) : super(key: key);
 
   @override
   State<ShowDetailsUI> createState() => _ShowDetailsUIState();
 }
 
 class _ShowDetailsUIState extends State<ShowDetailsUI> {
+  Future<void> _launchInBrowser(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    // Use `Uri` to ensure that `phoneNumber` is properly URL-encoded.
+    // Just using 'tel:$phoneNumber' would create invalid URLs in some cases,
+    // such as spaces in the input, which would cause `launch` to fail on some
+    // platforms.
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launch(launchUri.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        // backgroundColor: Colors.yellow,
+        title: Text(
+          "รายละเอียดร้าน",
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/" + widget.image_fs.toString(),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 50.0,
+                  right: 50.0,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.home,
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(
+                            widget.name_fs.toString(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _launchInBrowser(widget.web_fs.toString());
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.link,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              widget.web_fs.toString(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _launchInBrowser(
+                            'https://fb.com/' + widget.facebook_fs.toString());
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.facebook,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              widget.facebook_fs.toString(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _makePhoneCall(widget.phone_fs.toString());
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.phone,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              widget.phone_fs.toString(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _launchInBrowser(
+                            "https://www.google.com/maps/@${widget.lat_fs.toString()},${widget.lng_fs.toString()},15z");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.map,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              widget.lng_fs.toString(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/splas_screen_ui.dart
+++ lib/views/splas_screen_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_msu2/views/show_list_ui.dart';
 
 class SplasScreenUI extends StatefulWidget {
   const SplasScreenUI({Key? key}) : super(key: key);
@@ -8,10 +9,73 @@ class SplasScreenUI extends StatefulWidget {
 }
 
 class _SplasScreenUIState extends State<SplasScreenUI> {
+  void initState() {
+    // TODO: implement initState
+    // หน่วยเวลาหน้าจอ
+    Future.delayed(
+      Duration(
+        seconds: 5,
+      ),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ShowListUI(),
+        ),
+      ),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.grey,
+      // ),
+      backgroundColor: Colors.yellow,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/fastfood.png",
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "สายด่วนชวนกิน",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.yellow,
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 30.0,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  "Created by MSU 2022",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
