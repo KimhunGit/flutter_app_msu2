9474aeeb316e238b4bbb868dc080579461dfc506
KimhunGit <jakkrid.b@msu.ac.th>
2022-03-11 10:02:22 +0700

    Code V1.2:Create File Assets

---
+++ lib/views/show_details_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShowDetailsUI extends StatefulWidget {
+  const ShowDetailsUI({Key? key}) : super(key: key);
+
+  @override
+  State<ShowDetailsUI> createState() => _ShowDetailsUIState();
+}
+
+class _ShowDetailsUIState extends State<ShowDetailsUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
---
+++ lib/views/splas_screen_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplasScreenUI extends StatefulWidget {
+  const SplasScreenUI({Key? key}) : super(key: key);
+
+  @override
+  State<SplasScreenUI> createState() => _SplasScreenUIState();
+}
+
+class _SplasScreenUIState extends State<SplasScreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
